f13adf61da084e43d9e5376ea7bcbc18475d2912
mohamednourbenali <89775673+mohamednourbenali@users.noreply.github.com>
2024-11-11 17:58:43 +0100

    ajout des commentaires

--- src/Dropdown.js
+++ src/Dropdown.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
+// Composant Dropdown : un menu déroulant réutilisable avec un label, une liste d'options, et des props pour la valeur et le gestionnaire de changement.
 function Dropdown({ label, options, value, onChange }) {
   return (
     <div>
+      {/* Le label du menu déroulant */}
       <label>{label}</label>
+      {/* Création du menu déroulant */}
       <select value={value} onChange={onChange}>
+        {/* Parcours de la liste d'options pour créer chaque élément de sélection */}
         {options.map((option) => (
           <option key={option.value} value={option.value}>
             {option.label}
